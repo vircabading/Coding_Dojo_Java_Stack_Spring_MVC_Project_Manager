21bebdbdfb05acd4e61fe41b895e26ddfb409132
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-14 13:06:30 -0800

    add user

---
+++ src/main/java/com/vcabading/projectmanager/models/User.java
@@ -0,0 +1,89 @@
+package com.vcabading.projectmanager.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+/////////////////////////////////////////////////////////////
+//	USER CLASS
+/////////////////////////////////////////////////////////////
+
+@Entity
+@Table(name="users")
+public class User {
+
+	//	//// FIELDS /////////////////////////////////////////
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Pattern(regexp="^[A-Za-z]*$",message = "User Name must be letters only")		// Validate: Alphabet Only
+    @NotNull
+    @NotEmpty(message="First Name is required!")
+    @Size(min=3, max=30, message="Username must be between 3 and 30 characters")
+    private String firstName;
+    
+    @Pattern(regexp="^[A-Za-z]*$",message = "User Name must be letters only")		// Validate: Alphabet Only
+    
+    @NotNull
+    @NotEmpty(message="Last name is required!")
+    @Size(min=3, max=30, message="Username must be between 3 and 30 characters")
+    private String lastName;
+    
+    @NotNull
+    @NotEmpty(message="Email is required!")
+    @Email(message="Please enter a valid email!")
+    private String email;
+    
+    @NotNull
+    @NotEmpty(message="Password is required!")
+    @Size(min=8, max=128, message="Password must be between 8 and 128 characters")
+    private String password;
+    
+    @Transient
+    @NotEmpty(message="Confirm Password is required!")
+    @Size(min=8, max=128, message="Confirm Password must be between 8 and 128 characters")
+    private String confirm;
+    
+
+	@Column(updatable=false)		// this will not allow createdAt to be updated after creation
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	@PrePersist
+	protected void onCreate()	{
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+  
+	//	//// CONSTRUCTOR ////////////////////////////////////
+	
+    public User() {}
+
+    //	//// GETTERS AND SETTERS ////////////////////////////
+    
+    
+}
