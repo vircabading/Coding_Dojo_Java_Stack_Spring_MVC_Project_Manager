2ec83f8ee50a4ff469702be855d484814ad772d9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-14 18:53:01 -0800

    add ProjectSecurity

---
+++ src/main/java/com/vcabading/projectmanager/services/ProjectService.java
@@ -0,0 +1,62 @@
+package com.vcabading.projectmanager.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/////////////////////////////////////////////////////
+//	PROJECT SERVICE
+/////////////////////////////////////////////////////
+
+@Service
+public class ProjectService {
+
+	//	//// FIELDS /////////////////////////////////
+
+	@Autowired
+	ProjectRepository projectRepo;
+
+	//	//// CREATE /////////////////////////////////
+
+	//	---- Create a Project -----------------------
+	public Project create(Project project) {
+		return this.projectRepo.save(project);
+	}
+
+	//	//// RETRIEVE ///////////////////////////////
+
+	//	---- Retrieve All ---------------------------
+	public List<Project> retrieveAll() {
+		return this.projectRepo.findAll();
+	}
+
+	//	---- Retrieve Project by ID -----------------
+	public Project retrieveproject(Long id) {
+		Optional<Project> optProject = this.projectRepo.findById(id);
+		if (optProject.isPresent()) {
+			Project project = optProject.get();
+			return project;
+		} else {
+			return null;
+		}
+	}
+
+	//	//// UPDATE /////////////////////////////////
+
+	public Project update(Project project) {
+		Optional<Project> optProject = this.projectRepo.findById(project.getId());
+		if (optProject.isPresent()) {
+			return this.projectRepo.save(project);
+		} else {
+			return null;
+		}
+	}
+
+	//	//// DELETE /////////////////////////////////
+
+	public void delete(Long id) {
+		this.projectRepo.deleteById(id);
+	}
+}
