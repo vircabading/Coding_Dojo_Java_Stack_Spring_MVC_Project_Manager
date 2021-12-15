561a432bcc19341034c8035ca1e95ab92949c98d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-14 19:58:11 -0800

    add fields in ProjectController

--- src/main/java/com/vcabading/projectmanager/controllers/ProjectController.java
+++ src/main/java/com/vcabading/projectmanager/controllers/ProjectController.java
@@ -1,6 +1,10 @@
 package com.vcabading.projectmanager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.vcabading.projectmanager.services.ProjectService;
+import com.vcabading.projectmanager.services.UserService;
 
 ////////////////////////////////////////////////////////////////////
 //	PROJECT CONTROLLER
@@ -8,5 +12,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProjectController {
+	//	//// FIELDS ////////////////////////////////////////////////
 
+	@Autowired
+	private UserService userServ;
+
+	@Autowired
+	private ProjectService bookServ;
+
+	//	//// PROJECT SHOW //////////////////////////////////////////
 }
